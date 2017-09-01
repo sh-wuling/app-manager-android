20217db6913bec0bfabdf44c23a9d7fef600152d
周程程 <totzcc@163.com>
2017-09-01 19:11:33 +0800

    add DownloadFileProvider

---
+++ app-manager/src/main/java/me/wuling/components/manager/DownloadFileProvider.java
@@ -0,0 +1,11 @@
+package me.wuling.components.manager;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * DownloadFileProvider
+ * Created by zhoucheng on 2017/9/1.
+ */
+
+public class DownloadFileProvider extends FileProvider {
+}
